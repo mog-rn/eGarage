6f8f20a1f291f12cc8640c55f483c95b7c0af44e
Mogakamo <mogaka.amo254@gmail.com>
2022-03-06 20:14:36 +0300

    commit

--- web-client/pages/index.js
+++ web-client/pages/index.js
@@ -1,15 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default function Home() {
   return (
     <div className="">
       <Head>
         <title>eGarage</title>
-        <meta name="description" content="This a web application that helps locate garages easily" />
+        <meta
+          name="description"
+          content="This a web application that helps locate garages easily"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="font-thin">This is eGarage</h1>
+      kl
     </div>
-  )
+  );
 }
