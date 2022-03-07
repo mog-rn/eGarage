d9da5a08d3709ac60d8983af413420b7519e9353
Mogakamo <mogaka.amo254@gmail.com>
2022-03-07 11:54:45 +0300

    Added the mandatory packages for the backend server and written the server code

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:5050",
+};
+
+app.use(cors(corsOptions));
+
+// Parse requests of Content-Type -> application/json
+app.use(express.json());
+
+// Parse requests of Content-Type -> application/x-www-form-urlencoded
+app.use(express.urlencoded({ extend: true }));
+
+// Simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to eGarage's backend api" });
+});
+
+const PORT = process.env.PORT || 5050;
+
+// Setting the port to listen for requests
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
