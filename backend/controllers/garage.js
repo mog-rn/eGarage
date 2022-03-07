f12f2887e96c05ec70abab19f8c40793af5bb7e2
Mogakamo <mogaka.amo254@gmail.com>
2022-03-07 13:12:24 +0300

    Added the controller

---
+++ backend/controllers/garage.js
@@ -0,0 +1,31 @@
+const Garage = require("../models/garage")
+
+// Create and save a new garage
+exports.create = (req, res) => {
+
+}
+
+// Retrieve all Garages from the database
+exports.findAll = (req, res) => {
+
+}
+
+// Find a single garage with :id
+exports.findOne = (req, res) => {
+
+}
+
+// Update a garage identified by the {:id} in the request 
+exports.update = (req, res) => {
+
+}
+
+// Delete a garage by {:id} in the request 
+exports.delete = (req, res) => {
+
+}
+
+// Delete all the garages
+exports.deleteAll = (req, res) => {
+
+}
