cebe4ffa8cd754a07b79e82f62d4ccd39f57ca93
Mogakamo <mogaka.amo254@gmail.com>
2022-03-08 16:17:53 +0300

    SOLVED: Database connection issue

--- backend/models/db.js
+++ backend/models/db.js
@@ -3,6 +3,10 @@ const dbConfig = require("../config/db.config");
 
 // Connection to the database
 const connection = mysql.createPool({
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
@@ -10,7 +14,7 @@ const connection = mysql.createPool({
 });
 
 // Open the MySQL connection
-connection.connect((error) => {
+connection.getConnection((error) => {
   if (error) throw error;
   console.log("Successfully connected to the database...");
 });
