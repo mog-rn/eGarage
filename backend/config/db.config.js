bbf3b98df02d9dd6f141aab17ccb760edf13d5d7
Mogakamo <mogaka.amo254@gmail.com>
2022-03-08 12:50:17 +0300

    created the heroku database for the application

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "mogaka_dev",
-    DB: "egarage"
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "be8ab370f5c74f",
+    PASSWORD: "e4aae93e",
+    DB: "heroku_d0c14b8da4e83d7"
 }
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "mogaka_dev",
+//     DB: "egarage"
+// }
+
+:
