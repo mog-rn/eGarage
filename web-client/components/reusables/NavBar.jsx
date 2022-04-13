24247dd51f8ce25835eda7afec0fd8130c67f46f
Mogakamo <mogaka.amo254@gmail.com>
2022-04-13 17:46:04 +0300

    made the navbar links to work

--- web-client/components/reusables/NavBar.jsx
+++ web-client/components/reusables/NavBar.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
   return (
     <div className="sticky z-50 relative flex items-center justify-between h-16 w-full shadow-md p-10 bg-[#F6FBF2]">
-        {/* logo */}
-        <div className='ml-20'>
-            <h1 className='text-4xl text-[#118024] font-bold'>MyGarage</h1>
-        </div>
-        {/* Links */}
-        <div>
-            <ul className='flex text-lg flex-row space-x-20 items-end'>
-                <li>Home</li>
-                <li>Services</li>
-                <li>Contact Us</li>
-            </ul>
-        </div>
-        {/* Buttons */}
-        <div className='space-x-7 '>
-            <button className='text-[#118024] text-lg w-auto h-[45px]'>Sign in</button>
-            <button className='bg-[#118024] text-white text-lg p-4 w-auto rounded-md'>Register</button>
-        </div>
+      {/* logo */}
+      <div className="ml-20">
+        <h1 className="text-4xl text-[#118024] font-bold">
+            <Link href="/">MyGarage</Link>
+        </h1>
+      </div>
+      {/* Links */}
+      <div>
+        <ul className="flex text-lg flex-row space-x-20 items-end">
+          <Link href="/">Home</Link>
+          <Link href="/">Services</Link>
+          <Link href="/">Contact Us</Link>
+        </ul>
+      </div>
+      {/* Buttons */}
+      <div className="space-x-7 ">
+        <button className="text-[#118024] text-lg w-auto h-[45px]">
+          <Link href="/login">Sign in</Link>
+        </button>
+        <button className="bg-[#118024] text-white text-lg p-4 w-auto rounded-md">
+          <Link href="/register">Register</Link>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
