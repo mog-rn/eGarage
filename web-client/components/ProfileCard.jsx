ed89d5788bfa4ff3dd98cad5f45c233d99c24d43
Mogakamo <mogaka.amo254@gmail.com>
2022-04-13 14:46:02 +0300

    Working on the deployment

--- web-client/components/ProfileCard.jsx
+++ web-client/components/ProfileCard.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const ProfileCard = () => {
   return (
-    <div className='h-[150px] p-5 border-black border-4 rounded-3xl flex items-center justify-center flex-col'>
-        <h1 className='flex items-center'>Welcome, Mogaka {/**Added the hi emoji */}</h1>
-        <span>It's nice to see you back again...</span>
-    </div>
+    <></>
   )
 }
 
