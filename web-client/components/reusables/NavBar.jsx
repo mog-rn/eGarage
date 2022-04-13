57dc11d9dba7367a40982fcab15221c67439c301
Mogakamo <mogaka.amo254@gmail.com>
2022-04-13 15:07:03 +0300

    Added the poppins font and configured it in the project

--- web-client/components/reusables/NavBar.jsx
+++ web-client/components/reusables/NavBar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-between h-16 w-full shadow-md p-10 top-2 bg-[#F6FBF2]">
+    <div className="font-san flex items-center justify-between h-16 w-full shadow-md p-10 top-2 bg-[#F6FBF2]">
         {/* logo */}
         <div className='ml-20'>
             <h1 className='text-4xl text-[#118024] font-bold'>MyGarage</h1>
         </div>
         {/* Links */}
         <div>
-            <ul className='flex flex-row space-x-20 items-end'>
+            <ul className='flex text-lg flex-row space-x-20 items-end'>
                 <li>Home</li>
                 <li>Services</li>
                 <li>Contact Us</li>
@@ -17,8 +17,8 @@ const NavBar = () => {
         </div>
         {/* Buttons */}
         <div className='space-x-7 '>
-            <button className='text-[#118024] w-[55px] h-[45px]'>Sign in</button>
-            <button className='bg-[#118024] text-white p-4 w-24 rounded-md'>Register</button>
+            <button className='text-[#118024] text-lg w-auto h-[45px]'>Sign in</button>
+            <button className='bg-[#118024] text-white text-lg p-4 w-auto rounded-md'>Register</button>
         </div>
     </div>
   )
