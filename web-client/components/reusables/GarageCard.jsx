edfffba4704f4e61993fa5f3e014aafdb6f69ec5
Mogakamo <mogaka.amo254@gmail.com>
2022-04-17 17:53:54 +0300

    finished with the garage card

--- web-client/components/reusables/GarageCard.jsx
+++ web-client/components/reusables/GarageCard.jsx
@@ -7,7 +7,7 @@ const GarageCard = () => {
     <div className="p-10 space-y-3">
       <h1 className="text-4xl font-semibold">Popular garages near you</h1>
       <p>Lorem ipsum et luctus venenat</p>
-      <div class="max-w-sm space-y-8 rounded border-black overflow-hidden shadow-lg p-10 py-12">
+      <div class="max-w-sm space-y-8 rounded-lg border-black overflow-hidden shadow-lg p-10 bg-[#F6FBF2] py-12">
         <div className="flex items-center space-x-2">
           <img
             class="w-16 h-16 rounded-full"
@@ -17,9 +17,9 @@ const GarageCard = () => {
           />
           <div>
             {/* Garage name */}
-            <h1>Sahil Garage</h1>
+            <h1 className="text-lg font-light">Sahil Garage</h1>
             {/* Location */}
-            <div className="flex items-center space-x-1">
+            <div className=" text-lg font-extralight flex items-center space-x-1">
               <GrLocation className="" />
               <h2>Nairobi</h2>
             </div>
@@ -28,12 +28,14 @@ const GarageCard = () => {
         </div>
         <div className="space-y-2">
           {/* category */}
-          <h1 className="text-lg">Car Garage</h1>
-          <p className="text-sm">Lorem</p>
-          <p>Opening: 08:00</p>
-          <p>Closing: 18:00</p>
+          <h1 className="text-lg font-bold">Car Garage</h1>
+          <p className="text-sm">This is a car garage</p>
+          <div className="pt-8">
+          <p>Opening: 0800 hrs</p>
+          <p>Closing: 1800 hrs</p>
+          </div>
         </div>
-        <button className="bg-[#118024] text-white p-2 rounded-md">Find now</button>
+        <button className="bg-[#118024] text-transform: uppercase text-white p-2 rounded-md">Find now</button>
       </div>
     </div>
   );
