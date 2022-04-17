27d0274a44dabe947559f24fe1d80df5ea8f2d09
Mogakamo <mogaka.amo254@gmail.com>
2022-04-17 23:24:53 +0300

    too tired going to sleep but will solve the features section problem

--- web-client/components/Features.jsx
+++ web-client/components/Features.jsx
@@ -3,30 +3,32 @@ import { AiOutlineCar } from "react-icons/ai";
 
 const Features = () => {
   return (
-    <div className="flex w-5/6 px-10 space-x-10 mx-auto h-32 bg-[#EAE8DF] rounded-2xl items-center justify-between">
-      <div className="flex h-auto space-x-5">
-        {/* Car icon */}
-        <div className="icon h-auto flex space-x-5">
-          <AiOutlineCar className="h-20 w-20 bg-[#F6FBF2] rounded-lg p-4" />
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="font-bold text-[#118024] text-4xl">500+</h1>
-            <p className="text-lg text-transform: uppercase">car garages</p>
+    <div className="relative">
+      <div className="flex w-full px-10 mx-auto h-32 bg-[#EAE8DF] rounded-2xl items-center">
+        <div className="flex justify-between h-auto space-x-5">
+          {/* Car icon */}
+          <div className="flex space-x-5">
+            <AiOutlineCar className="h-20 w-20 bg-[#F6FBF2] rounded-lg p-4" />
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="font-bold text-[#118024] text-4xl">500+</h1>
+              <p className="text-lg text-transform: uppercase">car garages</p>
+            </div>
           </div>
-        </div>
-        {/* Bike icon */}
-        <div className="icon h-auto flex space-x-5">
-          <AiOutlineCar className="h-20 w-20 bg-[#F6FBF2] rounded-lg p-4" />
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="font-bold text-[#118024] text-4xl">500+</h1>
-            <p className="text-lg text-transform: uppercase">car garages</p>
+          {/* Bike icon */}
+          <div className="flex space-x-5">
+            <AiOutlineCar className="h-20 w-20 bg-[#F6FBF2] rounded-lg p-4" />
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="font-bold text-[#118024] text-4xl">300+</h1>
+              <p className="text-lg text-transform: uppercase">bike garages</p>
+            </div>
           </div>
-        </div>
-        {/* Usermechanic icon */}
-        <div className="icon h-auto flex space-x-5">
-          <AiOutlineCar className="h-20 w-20 bg-[#F6FBF2] rounded-lg p-4" />
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="font-bold text-[#118024] text-4xl">500+</h1>
-            <p className="text-lg text-transform: uppercase">car garages</p>
+          {/* Usermechanic icon */}
+          <div className="flex space-x-5">
+            <AiOutlineCar className="h-20 w-20 bg-[#F6FBF2] rounded-lg p-4" />
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="font-bold text-[#118024] text-4xl">4000+</h1>
+              <p className="text-lg text-transform: uppercase">mechanics</p>
+            </div>
           </div>
         </div>
       </div>
