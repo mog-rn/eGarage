06c5ed10cf5e8569ced68338b5cc05f21fbce0d4
Mogakamo <mogaka.amo254@gmail.com>
2022-04-19 11:07:25 +0300

    Finished with the registration page

--- web-client/pages/register.js
+++ web-client/pages/register.js
@@ -7,34 +7,34 @@ import Navbar from "../components/reusables/NavBar";
 
 const Register = () => {
   return (
-    <div className="h-screen">      {" "}
+    <div className="h-screen">
+      {" "}
       {/* <div className="pb-1"><Navbar /></div> */}
-      <div className="bg-[#EAE8DF] flex h-screen overflow-hidden px-12 scrollbar-hide py-16 space-y-5">
+      <div className="bg-[#EAE8DF] flex h-screen overflow-hidden px-60 scrollbar-hide py-10 space-y-5">
         <Head>
           <title>Create an Account</title>
         </Head>
         <div className="container w-full shadow-green-600 flex flex-row justify-items-center mx-auto rounded-3xl bg-white">
           {/* right */}
           <div className="relative w-1/2 rounded-3xl h-auto">
-            <img
-              src="https://avatars.githubusercontent.com/u/61131314?v=4"
+            <Image
+              src="/images/car.jpg"
               alt="image"
+              layout="fill"
+              objectFit="cover"
               className="h-full w-full rounded-3xl"
             />
             <div
               className="text-white space-y-8 absolute top-10 p-10 bg-[#118024]/25 bg-opacity-80 rounded-xl
-                   h-5/6 ml-16 w-5/6  mr-16 px-16 bg-clip-padding backdrop-blur-sm"
-              filter={"blur(10px) sepia(50%)"}
-            >
-              <h1 className="">MyGarage</h1>
-              <h2>Lorem ipsum mou</h2>
-              <p>
-                Lorem ipsum dolor sit amet, con commodo sed egestas egestas
-                fringilla phasellus faucibus
+                   h-5/6 ml-10 w-5/6 px-8 bg-clip-padding backdrop-blur-sm">
+              <h1 className="text-4xl">MyGarage</h1>
+              <h2 className="text-xl">Create an account</h2>
+              <p className="text-sm">
+                Create an account to get started with MyGarage. You can also get to have a personalized user experience while using the application.
               </p>
               <div className="bottom-5 absolute">
                 <p>Already have an account?</p>
-                <button>
+                <button className="decoration: underline underline-offset-2">
                   <Link href="/login">Sign in</Link>
                 </button>
               </div>
