c7cc46844aeee44bf83ea8140bcfccdc97fe1f3e
Mogakamo <mogaka.amo254@gmail.com>
2022-04-23 21:49:36 +0300

    Created the wrapper

--- web-client/components/authentication/wrapper.jsx
+++ web-client/components/authentication/wrapper.jsx
@@ -1,13 +1,28 @@
-import * as React from 'react'
-import {useSession} from "next-auth/react"
-import Link from 'next/link'
-import {useRouter} from "next/router"
+import * as React from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
+const Wrapper = (props) => {
+  const session = useSession();
+  const router = useRouter();
 
-const Wrapper = () => {
-  return (
-    <div>Wrapper</div>
-  )
-}
+  if (
+    (session !== null && session?.status === "authenticated") ||
+    router.pathname === "/" ||
+    router.pathname === "/register"
+  ) {
+    return props.children;
+  } else {
+    return (
+      <>
+        <h1>You are not authenticated</h1>
+        <Link href="/login">Back to Login</Link>
+      </>
+    )
+  }
 
-export default Wrapper
+  
+};
+
+export default Wrapper;
