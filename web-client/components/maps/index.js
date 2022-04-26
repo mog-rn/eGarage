ee1442ceaf56cb453201f71561701863024204f7
Mogakamo <mogaka.amo254@gmail.com>
2022-04-26 11:42:29 +0300

    Created the maps component

---
+++ web-client/components/maps/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MapsComponent = () => {
+  return (
+    <div>MapsComponent</div>
+  )
+}
+
+export default MapsComponent
---
+++ web-client/pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MapsComponent from '../components/maps'
+
+const Search = () => {
+  return (
+    <div>
+        <MapsComponent />
+    </div>
+  )
+}
+
+export default Search
