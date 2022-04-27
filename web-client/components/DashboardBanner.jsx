15bcb4059469e530a8c55bef278092fbe39a40d4
Mogakamo <mogaka.amo254@gmail.com>
2022-04-27 05:10:24 +0300

    added the line height to my dashboard banner text

--- web-client/components/DashboardBanner.jsx
+++ web-client/components/DashboardBanner.jsx
@@ -4,7 +4,7 @@ const DashboardBanner = () => {
   return (
     <div className='h-48 bg-gradient-to-r from-[#118024]/10  to-[#118024]/60
     p-10 '>
-        <h1 className='text-4xl w-[400px] text-[#118024]'>A genuine solution to your vehicle problems</h1>
+        <h1 className='text-4xl w-[400px] text-[#118024] leading-relaxed'>A genuine solution to your vehicle problems</h1>
         <div>
             {/* Image */}
         </div>
