d6add7e0237636ac7089b6e5fc0328c9aab2319b
Mogakamo <mogaka.amo254@gmail.com>
2022-04-27 05:09:09 +0300

    added the to do component for th eimage

--- web-client/components/DashboardBanner.jsx
+++ web-client/components/DashboardBanner.jsx
@@ -5,6 +5,9 @@ const DashboardBanner = () => {
     <div className='h-48 bg-gradient-to-r from-[#118024]/10  to-[#118024]/60
     p-10 '>
         <h1 className='text-4xl w-[400px] text-[#118024]'>A genuine solution to your vehicle problems</h1>
+        <div>
+            {/* Image */}
+        </div>
     </div>
   )
 }
