4d3ca4a9ab418320ab409670afdf810f0c54bb69
Mogakamo <mogaka.amo254@gmail.com>
2022-04-27 04:52:29 +0300

    commit

--- web-client/components/DashboardBanner.jsx
+++ web-client/components/DashboardBanner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DashboardBanner = () => {
   return (
-    <div className='h-48 bg-gradient-to-r from-cyan-100 via-purple-600 to-green-800'>DashboardBanner</div>
+    <div className='h-48 bg-gradient-to-r from-cyan-100  to-[#118024]'>DashboardBanner</div>
   )
 }
 
