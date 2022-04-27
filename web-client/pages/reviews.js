b26b1986d28b9ccc3ecf231bbc73c4798b050a29
Mogakamo <mogaka.amo254@gmail.com>
2022-04-27 05:19:32 +0300

    working on the reviews page

---
+++ web-client/pages/reviews.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../components/reusables/SideBar'
+
+const Reviews = () => {
+  return (
+    <div className='flex h-screen bg-[#F6FBF2]'>
+        <SideBar />
+
+        <div className='p-10 flex-grow'>Center</div>
+    </div>
+  )
+}
+
+export default Reviews
