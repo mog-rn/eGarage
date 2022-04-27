970ec68ea7699d8a3fa81557fb0de0ea1bc42b06
Mogakamo <mogaka.amo254@gmail.com>
2022-04-27 05:08:35 +0300

    Finished with the banner but havent added the image

--- web-client/components/DashboardBanner.jsx
+++ web-client/components/DashboardBanner.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const DashboardBanner = () => {
   return (
-    <div className='h-48 bg-gradient-to-r from-cyan-100  to-[#118024]'>DashboardBanner</div>
+    <div className='h-48 bg-gradient-to-r from-[#118024]/10  to-[#118024]/60
+    p-10 '>
+        <h1 className='text-4xl w-[400px] text-[#118024]'>A genuine solution to your vehicle problems</h1>
+    </div>
   )
 }
 
--- web-client/pages/dashboard.js
+++ web-client/pages/dashboard.js
@@ -3,10 +3,14 @@ import DashboardBanner from "../components/DashboardBanner";
 import DashboardSearch from "../components/DashboardSearch";
 import SideBar from "../components/reusables/SideBar";
 import UserView from "../components/UserView";
+import Head from "next/head";
 
 const Dashboard = () => {
   return (
     <div className="flex bg-[#F6FBF2] h-screen w-screen">
+      <Head>
+        <title>Dashboard</title>
+      </Head>
       <SideBar />
 
       <div className="p-5 flex-grow">
