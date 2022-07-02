3f7d5c446a2746ca4c08f091ad2e2d77e4e43140
Mogakamo <mogaka.amo254@gmail.com>
2022-07-02 19:09:17 +0300

    Building a scalable app

--- web-client/components/GarageOwner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GarageOwner = () => {
-  return (
-    <div>GarageOwner</div>
-  )
-}
-
-export default GarageOwner
--- web-client/components/ProfileCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProfileCard = () => {
-  return (
-    <></>
-  )
-}
-
-export default ProfileCard
