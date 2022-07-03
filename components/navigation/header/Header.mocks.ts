348a8d7aa87c5f06c64247f231dec184fae9ad2b
Mogakamo <mogaka.amo254@gmail.com>
2022-07-03 12:18:08 +0300

    rfc: Header component

---
+++ components/navigation/header/Header.mocks.ts
@@ -0,0 +1,9 @@
+import { IHeader } from './Header';
+
+const base: IHeader = {
+  
+};
+
+export const mockHeaderProps = {
+  base,
+};
---
+++ components/navigation/header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+export interface IHeader {
+  
+}
+
+const Header: React.FC<IHeader> = ({  }) => {
+  return <div className={styles.container}></div>;
+};
+
+export default Header;
