87c117b327b1b405e097c0f6c41a28d70cfd6173
Mogakamo <mogaka.amo254@gmail.com>
2022-07-03 12:22:26 +0300

    rfc: Header component

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -1,20 +1,27 @@
-import Head from "next/head";
+import Head from 'next/head';
+import { Header } from '../../../stories/Header';
 
 export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {
-    justify?: 'items-center' | 'items-start'
+  justify?: 'items-center' | 'items-start';
 }
 
-const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children, justify = 'items-center', ...divProps }) => {
-    return (
-        <>
-            <Head>
-                <title>eGarage</title>
-            </Head>
-            <div className={`min-h-screen flex flex-col ${justify}`} {...divProps}>
-            <main className="">{children}</main>
-            </div>
-        </>
-    )
-}
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({
+  children,
+  justify = 'items-center',
+  ...divProps
+}) => {
+  return (
+    <>
+      <Head>
+        <title>eGarage</title>
+      </Head>
+      <div className={`min-h-screen flex flex-col ${justify}`} {...divProps}>
+        <Header>
+          <main className="">{children}</main>
+        </Header>
+      </div>
+    </>
+  );
+};
 
 export default PrimaryLayout;
--- components/navigation/header/Header.tsx
+++ components/navigation/header/Header.tsx
@@ -1,11 +1,11 @@
 import styles from './Header.module.css';
 
 export interface IHeader {
-  
+  children: React.ReactNode
 }
 
-const Header: React.FC<IHeader> = ({  }) => {
-  return <div className={styles.container}></div>;
+const Header: React.FC<IHeader> = ({ children }) => {
+  return <div className={styles.container}>Header</div>;
 };
 
 export default Header;
