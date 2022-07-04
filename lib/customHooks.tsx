a12aecddd3ad9a3f68780e04f9d2928f799b8f67
Mogakamo <mogaka.amo254@gmail.com>
2022-07-04 09:13:25 +0300

    build: authentication

---
+++ lib/customHooks.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import {getAuthenticatedUser} from "./common"
+import {APP_ROUTES} from "../utils/constants"
+import Router from 'next/router'
+
+export const useUser = () => {
+    const [user, setUser] = useState(null)
+    const [authenticated, setAuthenticated] = useState(false)
+
+    useEffect(() => {
+        async function getUserDetails() {
+            const {authenticated, user} = await getAuthenticatedUser()
+            if (!authenticated) {
+                Router.push(APP_ROUTES.SIGN_IN)
+                return
+            }
+
+            setUser(user)
+            setAuthenticated(authenticated)
+
+        }
+        getUserDetails();
+    }, [])
+
+    return {user, authenticated}
+}
