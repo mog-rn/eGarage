bc7f01f33ff81d9fb4fdc645070bb1bde330966b
Mogakamo <mogaka.amo254@gmail.com>
2022-07-04 10:17:28 +0300

    build: signin form

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -50,14 +50,29 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-white">
       <div
-        className="container border-2 h-72 md:w-[600px] rounded-md 
+        className="container border-2 h-84 md:w-[600px] rounded-md 
        shadow-md grid grid-cols-2"
       >
         {/* Image */}
         <div></div>
-        <div>
-            <input type="email" name="" id="" />
-        </div>
+        <form className='flex flex-col space-y-3 py-3 items-center justify-start'>
+            <h1 className='font-bold text-lg'>Sign In</h1>
+            <p className='text-xs text-green text-opacity-80'>Welcome back to eGarage!</p>
+          <div className='flex flex-col'>
+            <label className='text-sm' htmlFor="email">Email</label>
+            <input type="email" name="" id="" className='w-72 h-8 rounded-xl outline-none focus:outline-none p-5 text-gray-400 focus:border-gray-400 border-2 border-transparent
+            transition duration-500 ease-in-out ' />
+          </div>
+          <div>
+            <label className='text-sm' htmlFor="password">Password</label>
+            <input type="password" name="" id="" className='w-72 h-8 rounded-xl outline-none focus:outline-none p-5 text-gray-400 focus:border-gray-400 border-2 border-transparent
+            transition duration-500 ease-in-out ' />
+          </div>
+          {/* Login buttons */}
+          <div>
+            <button className='bg-green px-5 py-2 text-[#fff] rounded-lg'>Login</button>
+          </div>
+        </form>
       </div>
     </div>
   );
