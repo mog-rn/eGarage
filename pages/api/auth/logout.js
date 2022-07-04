8eb28252825cb123be1294bff2a458a1464142ac
Mogakamo <mogaka.amo254@gmail.com>
2022-07-04 08:46:26 +0300

    build: logout API

---
+++ pages/api/auth/logout.js
@@ -0,0 +1,15 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+
+const cookie = {
+    cookie: process.env.COOKIE_NAME,
+    password: process.env.COOKIE_PASSWORD,
+    cookieOptions: {secure: process.env.NODE_ENV === 'production'}
+}
+
+export default withIronSessionApiRoute(
+    function signOut(req, res) {
+        req.session.destroy()
+        req.statusCode(200).json({ok: true})
+    },
+    cookie
+)
