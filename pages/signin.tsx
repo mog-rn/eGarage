09c35232c084145afc6c2b8a6eb4bf7876de3a1f
Mogakamo <mogaka.amo254@gmail.com>
2022-07-04 10:50:38 +0300

    build: signin form

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -51,26 +51,56 @@ const Login = () => {
     <div className="flex items-center justify-center h-screen bg-white">
       <div
         className="container border-2 h-84 md:w-[600px] rounded-md 
-       shadow-md grid grid-cols-2"
+       shadow-md grid grid-cols-2 px-4 mx-auto"
       >
         {/* Image */}
         <div></div>
-        <form className='flex flex-col space-y-3 py-3 items-center justify-start'>
-            <h1 className='font-bold text-lg'>Sign In</h1>
-            <p className='text-xs text-green text-opacity-80'>Welcome back to eGarage!</p>
-          <div className='flex flex-col'>
-            <label className='text-sm' htmlFor="email">Email</label>
-            <input type="email" name="" id="" className='w-72 h-8 rounded-xl outline-none focus:outline-none p-5 text-gray-400 focus:border-gray-400 border-2 border-transparent
-            transition duration-500 ease-in-out ' />
+        <form className="flex flex-col space-y-3 py-8 items-center justify-start">
+          <h1 className="font-bold text-lg">Sign In</h1>
+          <p className="text-xs text-green text-opacity-80">
+            Welcome back to eGarage!
+          </p>
+          <div className='flex flex-col space-y-5 justify-center'>
+          <div className="flex flex-col">
+            <label className="text-sm" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="w-64 text-start h-2 rounded-xl outline-none focus:outline-none p-5 text-gray-400 focus:border-gray-400 border-2 border-transparent
+            transition duration-500 ease-in-out "
+            />
           </div>
-          <div>
-            <label className='text-sm' htmlFor="password">Password</label>
-            <input type="password" name="" id="" className='w-72 h-8 rounded-xl outline-none focus:outline-none p-5 text-gray-400 focus:border-gray-400 border-2 border-transparent
-            transition duration-500 ease-in-out ' />
+          <div className='items-center'>
+            <label className="text-sm" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="w-64 h-8 rounded-xl outline-none focus:outline-none p-5 text-gray-400 focus:border-gray-400 border-2 border-transparent
+            transition duration-500 ease-in-out "
+            />
+          </div>
           </div>
           {/* Login buttons */}
           <div>
-            <button className='bg-green px-5 py-2 text-[#fff] rounded-lg'>Login</button>
+            <button
+              onClick={signIn}
+              className="bg-green px-5 py-2 text-[#fff] rounded-lg"
+            >
+              {
+                // isLoading ? <Loading /> : null
+              }
+              <span>Login</span>
+            </button>
           </div>
         </form>
       </div>
