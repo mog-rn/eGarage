17a05ed278f16d2051045fc354dcb147edfee4d9
Mogakamo <mogaka.amo254@gmail.com>
2022-07-05 17:29:53 +0300

    build: DashboardBanner component

--- components/dashboard/dashboardBanner/DashboardBanner.tsx
+++ components/dashboard/dashboardBanner/DashboardBanner.tsx
@@ -1,7 +1,9 @@
 export interface IDashboardBanner {}
 
 const DashboardBanner: React.FC<IDashboardBanner> = () => {
-  return <div className="">DashboardBanner</div>;
+  return <div className="border-2 md:h-48 md:w-[100vh] bg-white">
+    <h1 className="text-6xl w-auto text-green">A genuine solution to all your vehicle problems</h1>
+  </div>;
 };
 
 export default DashboardBanner;
