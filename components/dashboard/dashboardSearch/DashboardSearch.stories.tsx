ec8f75a97d8f70ce0e2722a2415950e8210b45a0
Mogakamo <mogaka.amo254@gmail.com>
2022-07-05 17:20:21 +0300

    build: Dashboard Search and Banner component

--- components/dashboard/dashboardSearch/DashboardSearch.stories.tsx
+++ components/dashboard/dashboardSearch/DashboardSearch.stories.tsx
@@ -1,22 +1,22 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import DashboardBanner, { IDashboardBanner } from './DashboardBanner';
-import { mockDashboardBannerProps } from './DashboardBanner.mocks';
+import DashboardSearch, { IDashboardSearch } from './DashboardSearch';
+import { mockDashboardSearchProps } from './DashboardSearch.mocks';
 
 export default {
-  title: 'templates/DashboardBanner',
-  component: DashboardBanner,
+  title: 'templates/DashboardSearch',
+  component: DashboardSearch,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
-} as ComponentMeta<typeof DashboardBanner>;
+} as ComponentMeta<typeof DashboardSearch>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof DashboardBanner> = (args) => (
-  <DashboardBanner {...args} />
+const Template: ComponentStory<typeof DashboardSearch> = (args) => (
+  <DashboardSearch {...args} />
 );
 
 export const Base = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 
 Base.args = {
-  ...mockDashboardBannerProps.base,
-} as IDashboardBanner;
+  ...mockDashboardSearchProps.base,
+} as IDashboardSearch;
