bce170b37f30b8b8997df267c6f563bfe0d29af6
Mogakamo <mogaka.amo254@gmail.com>
2022-07-16 19:55:16 +0300

    Added the layout structure for all the user pages

--- components/loading/Loading.tsx
+++ components/loading/Loading.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { CSSProperties, useState } from 'react';
+import { BeatLoader } from 'react-spinners';
+
+const override: CSSProperties = {
+  display: 'block',
+  margin: '0 auto',
+  borderColor: '#fff',
+};
 
 const Loading = () => {
-  return <div>Loading</div>;
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState('#fff');
+
+  return <BeatLoader color={color} loading={loading} cssOverride={override} size={5} />
 };
 
 export default Loading;
