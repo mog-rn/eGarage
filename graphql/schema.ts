0739ded95433e9d4fb4a6a1ca9fad27c4077e29b
Mogakamo <mogaka.amo254@gmail.com>
2022-07-18 16:35:47 +0300

    build: finished with GraphQL types

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,9 +1,10 @@
 import { makeSchema } from "nexus";
 import { join } from "path";
+import * as types from "./types";
 
 
 export const schema = makeSchema({
-    types: [],
+    types,
     outputs: {
         typegen: join(process.cwd(), 'node_modules', '@types', 'nexus-typegen', 'index.d.ts'),
         schema: join(process.cwd(), 'src', 'schema.graphql'),
---
+++ graphql/types/User.ts
@@ -0,0 +1,32 @@
+import { enumType, objectType } from 'nexus';
+import { Garage } from './Garage';
+
+export const User = objectType({
+  name: 'User',
+  definition(t: any) {
+    t.string('user_id');
+    t.string('firstname');
+    t.string('lastname');
+    t.string('email');
+    t.string('password');
+    t.string('profileimg');
+    t.string('user_description');
+    t.string('role', { type: Role });
+    t.string('user_created_at');
+    t.field('garage', {
+      type: Garage,
+      async resolve(_parent: any, _args: any, ctx: any) {
+        return await ctx.prisma.user.findUnique({
+          where: {
+            email: _parent.user_email,
+          },
+        });
+      },
+    });
+  },
+});
+
+const Role = enumType({
+  name: 'Role',
+  members: ['ADMIN', 'AUTOMOBILE_USER', 'GARAGE_OWNER'],
+});
