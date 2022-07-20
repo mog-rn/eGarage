cfd25f39345d2499ac8259bf364ece1d120ff403
Mogakamo <mogaka.amo254@gmail.com>
2022-07-20 14:48:20 +0300

    build: setting auth0 up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextPageWithLayout } from './page';
 import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 import apolloClient from '../lib/apollo';
 
@@ -12,10 +13,12 @@ interface AppPropsWithLayout extends AppProps {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <ApolloProvider client={apolloClient}>
-      getLayout(
-      <Component {...pageProps} /> )
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        getLayout(
+        <Component {...pageProps} /> )
+      </ApolloProvider>
+    </UserProvider>
   );
 }
 
