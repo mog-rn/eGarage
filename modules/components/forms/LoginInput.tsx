aa328b315970a67096f3b40a086099f227fcbe05
Mogakamo <mogaka.amo254@gmail.com>
2022-07-22 10:34:43 +0300

    build: Created the reusable form component

--- modules/components/forms/LoginInput.tsx
+++ modules/components/forms/LoginInput.tsx
@@ -1,54 +1,18 @@
-import { Field, Form } from 'formik';
+import { ErrorMessage, Field, FieldInputProps, Form, useField } from 'formik';
 import React from 'react';
 
-interface ILoginInput {}
+type Props = { name: string; label: string };
 
-export const LoginInput: React.FC<ILoginInput> = () => {
+export const LoginInput: React.FC<Props> = ({ name, label, ...props }) => {
+  const [field, meta] = useField({ name });
   return (
     <>
       <div className="flex space-x-3">
-        <div>
-          <label htmlFor="firstname" className="">
-            First Name
-          </label>
-          <Field
-            name="firstname"
-            type="text"
-            placeholder="Enter your first name"
-          />
-        </div>
-        <div>
-          <label htmlFor="lastname">Last Name</label>
-          <Field
-            name="lastname"
-            type="text"
-            placeholder="Enter your last name"
-          />
-        </div>
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Field name="email" type="email" placeholder="Enter your email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <Field
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-        />
-      </div>
-      <div>
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <Field
-          name="confirmPassword"
-          type="password"
-          placeholder="Confirm your password"
-        />
-      </div>
-      <div>
-        <label htmlFor="role">Role</label>
-        <Field name="role" type="text" placeholder="Enter your role" />
+        <label className="font-semibold">{label}</label>
+        <Field {...props} {...field} className="focus:border-green" />
+        {meta.touched && meta.error ? (
+          <ErrorMessage name={name} component="div" className="text-red-500" />
+        ) : null}
       </div>
     </>
   );
