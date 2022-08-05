aa3bcae447f71c4bbf62c6c96e5b128aff63bd66
Mogakamo <mogaka.amo254@gmail.com>
2022-08-05 09:25:02 +0300

    fix: added the secret key for next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,5 +10,11 @@ export default NextAuth({
             clientId: `${process.env.GOOGLE_AUTH_ID}`,
             clientSecret: `${process.env.GOOGLE_AUTH_SECRET}`,
         })
-    ]
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    // pages: {
+    //     signIn: "/auth",
+    //     signOut: "/auth",
+
+    // }
 })
