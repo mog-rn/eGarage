404dd92bd6e03718e86aeac22fc9f319bd2dc5b0
Mogakamo <mogaka.amo254@gmail.com>
2022-08-05 16:37:57 +0300

    fix: authenticated the pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,25 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from "@/components/layouts/index";
+import { getSession } from 'next-auth/react';
+
+export async function getServerSideProps(ctx: any) {
+  const session = await getSession(ctx)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
+
 
 const Home: NextPage = () => {
   return (
@@ -17,5 +36,7 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+
 
 export default Home
--- pages/review.tsx
+++ pages/review.tsx
@@ -1,5 +1,24 @@
 import PrimaryLayout from '@/components/layouts'
+import { getSession } from 'next-auth/react'
 import React from 'react'
+
+export async function getServerSideProps(ctx: any) {
+  const session = await getSession(ctx)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
+
 
 const Review = () => {
   return (
