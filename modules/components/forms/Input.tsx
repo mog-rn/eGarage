c77ee9a8976b627a289de8fb2777fbdf80f59e7d
Mogakamo <mogaka.amo254@gmail.com>
2022-08-12 08:48:14 +0300

    commit

--- modules/components/forms/Input.tsx
+++ modules/components/forms/Input.tsx
@@ -1,15 +1,27 @@
-import { useField } from "formik";
+import { useField, FieldHookConfig } from "formik";
 import classNames from "classnames";
-import React from "react";
+import React, {
+  ClassAttributes,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  TextareaHTMLAttributes,
+} from "react";
 import { ExclamationCircleIcon } from "@heroicons/react/solid";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
-const Input = ({ type = "", label = "", className = "", ...props }) => {
+const Input = (
+  { type = "", label = "", className = "" },
+  props: InputHTMLAttributes<HTMLInputElement> &
+    ClassAttributes<HTMLInputElement> &
+    FieldHookConfig<string> &
+    TextareaHTMLAttributes<string> &
+    DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+) => {
   const [field, meta] = useField(props);
   const error = meta?.touched && meta?.error;
 
   return (
-    <div className={classNames(className, 'flex flex-col space-y-1')}>
+    <div className={classNames(className, "flex flex-col space-y-1")}>
       {label ? (
         <label htmlFor="email" className="text-gray-600">
           {label}
@@ -17,16 +29,18 @@ const Input = ({ type = "", label = "", className = "", ...props }) => {
       ) : null}
       <div className="flex-1">
         {type === "textarea" ? (
-          <textarea
-            {...field}
-            {...props}
-            className={classNames(
-              "w-full shadow-sm rounded-md py-2 pl-4 truncate border focus:outline-none focus:ring-4 focus:ring-opacity-20 transition disabled:opacity-50 disabled:cursor-not-allowed",
-              error
-                ? "border-red-400 text-red-800 focus:border-red-400 focus:ring-red-400"
-                : "border-gray-300 focus:border-gray-400 focus:ring-gray-400"
-            )}
-          />
+          <>
+          </>
+          // <textarea
+          //   {...field}
+          //   {...props}
+          //   className={classNames(
+          //     "w-full shadow-sm rounded-md py-2 pl-4 truncate border focus:outline-none focus:ring-4 focus:ring-opacity-20 transition disabled:opacity-50 disabled:cursor-not-allowed",
+          //     error
+          //       ? "border-red-400 text-red-800 focus:border-red-400 focus:ring-red-400"
+          //       : "border-gray-300 focus:border-gray-400 focus:ring-gray-400"
+          //   )}
+          // />
         ) : (
           <div className="relative">
             <input
@@ -40,7 +54,7 @@ const Input = ({ type = "", label = "", className = "", ...props }) => {
                   : "border-gray-300 focus:border-gray-400 focus:ring-gray-400"
               )}
             />
-            {error && type !== 'number' ? (
+            {error && type !== "number" ? (
               <span className="pr-2 absolute right-0 top-1/2 -translate-y-1/2">
                 <ExclamationCircleIcon className="w-6 h-6 text-red-500" />
               </span>
@@ -49,20 +63,17 @@ const Input = ({ type = "", label = "", className = "", ...props }) => {
         )}
       </div>
 
-
       {error ? (
-        <p className="text-red-600 text-sm first-letter:uppercase">
-          {error}
-        </p>
+        <p className="text-red-600 text-sm first-letter:uppercase">{error}</p>
       ) : null}
     </div>
   );
 };
 
 Input.propTypes = {
-    type: PropTypes.string.isRequired,
-    label: PropTypes.string,
-    className: PropTypes.string,
-}
+  type: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  className: PropTypes.string,
+};
 
 export default Input;
