76ca80ed013cc454b9eb73dfef2c148d1ba59c79
Mogakamo <mogaka.amo254@gmail.com>
2022-08-12 08:52:54 +0300

    commit

--- modules/components/forms/Input.tsx
+++ modules/components/forms/Input.tsx
@@ -29,18 +29,16 @@ const Input = (
       ) : null}
       <div className="flex-1">
         {type === "textarea" ? (
-          <>
-          </>
-          // <textarea
-          //   {...field}
-          //   {...props}
-          //   className={classNames(
-          //     "w-full shadow-sm rounded-md py-2 pl-4 truncate border focus:outline-none focus:ring-4 focus:ring-opacity-20 transition disabled:opacity-50 disabled:cursor-not-allowed",
-          //     error
-          //       ? "border-red-400 text-red-800 focus:border-red-400 focus:ring-red-400"
-          //       : "border-gray-300 focus:border-gray-400 focus:ring-gray-400"
-          //   )}
-          // />
+          <input
+            {...field}
+            {...props}
+            className={classNames(
+              "w-full shadow-sm rounded-md py-2 pl-4 truncate border focus:outline-none focus:ring-4 focus:ring-opacity-20 transition disabled:opacity-50 disabled:cursor-not-allowed",
+              error
+                ? "border-red-400 text-red-800 focus:border-red-400 focus:ring-red-400"
+                : "border-gray-300 focus:border-gray-400 focus:ring-gray-400"
+            )}
+          />
         ) : (
           <div className="relative">
             <input
