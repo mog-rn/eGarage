2eeea82d92b0d28ba23ff78c6d020ca21f822e47
mogakamo <mogaka.amo254@gmail.com>
2022-09-11 08:06:55 +0300

    package: react-hook-form

---
+++ src/pages/Register.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+function RegisterPage() {
+
+    const {} = useForm()
+
+    return <>
+        <form action="">
+
+        </form>
+        <Link href="/login">Login</Link>
+    </>
+}
+
+export default RegisterPage;
