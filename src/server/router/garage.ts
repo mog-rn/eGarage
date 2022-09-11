db59669f27253046193320237cce0f2e123c5cd8
mogakamo <mogaka.amo254@gmail.com>
2022-09-11 16:23:38 +0300

    chore: garages api endpoint is complete

--- src/server/router/garage.ts
+++ src/server/router/garage.ts
@@ -1,6 +1,45 @@
+import { getSingleGarageSchema } from "./../../schema/garage.schema";
+import { createGarageSchema } from "../../schema/garage.schema";
 import { createRouter } from "./context";
+import * as trpc from "@trpc/server";
 
 export const garageRouter = createRouter()
-    .mutation('create-garage', {})
-    .query('garages', {})
-    .query('single-garage', {})
+  .mutation("create-garage", {
+    input: createGarageSchema,
+    async resolve({ ctx, input }) {
+      if (!ctx.user) {
+        return new trpc.TRPCError({
+          code: "FORBIDDEN",
+          message: "You must be logged in to create a garage",
+        });
+      }
+
+      const garage = await ctx.prisma.garage.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.user.id,
+            },
+          },
+        },
+      });
+
+      return garage;
+    },
+  })
+  .query("garages", {
+    async resolve({ ctx }) {
+      return ctx.prisma.garage.findMany();
+    },
+  })
+  .query("single-garage", {
+    input: getSingleGarageSchema,
+    async resolve({ input, ctx }) {
+      return ctx.prisma.garage.findUnique({
+        where: {
+          id: input.garageId,
+        },
+      });
+    },
+  });
