f699213b17b17caed274b78aa84c5e6d02067f2a
mogakamo <mogaka.amo254@gmail.com>
2022-09-11 09:01:57 +0300

    schema: update of the tables

---
+++ prisma/migrations/20220911055446_login_token/migration.sql
@@ -0,0 +1,62 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `updatedAt` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "Account_provider_providerAccountId_key";
+
+-- DropIndex
+DROP INDEX "Session_sessionToken_key";
+
+-- DropIndex
+DROP INDEX "VerificationToken_identifier_token_key";
+
+-- DropIndex
+DROP INDEX "VerificationToken_token_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Account";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Session";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "VerificationToken";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "LoginToken" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "redirect" TEXT NOT NULL DEFAULT '/',
+    CONSTRAINT "LoginToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" DATETIME,
+    "image" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_users" ("createdAt", "email", "emailVerified", "id", "image", "name") SELECT "createdAt", "email", "emailVerified", "id", "image", "name" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
