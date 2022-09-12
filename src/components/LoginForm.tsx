6ab9bc7dadd1117f7d5bb5353b734addcc36e1a3
mogakamo <mogaka.amo254@gmail.com>
2022-09-12 13:27:55 +0300

    finished with the login page styling

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,9 +4,13 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { CreateUserInput } from "../schema/user.schema";
 import { trpc } from "../utils/trpc";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/outline";
 
 function VerifyToken({ hash }: { hash: string }) {
   const router = useRouter();
+
   const { data, isLoading } = trpc.useQuery([
     "users.verify-otp",
     {
@@ -24,6 +28,8 @@ function VerifyToken({ hash }: { hash: string }) {
 }
 
 function LoginForm() {
+  const notify = () => toast.success("Check your email for the OTP");
+
   const { handleSubmit, register } = useForm<CreateUserInput>();
   const [success, setSuccess] = useState(false);
   const router = useRouter();
@@ -31,6 +37,7 @@ function LoginForm() {
   const { mutate, error } = trpc.useMutation(["users.request-otp"], {
     onSuccess: () => {
       setSuccess(true);
+      notify();
     },
   });
 
@@ -45,31 +52,52 @@ function LoginForm() {
   }
 
   return (
-    <div
-      className="bg-white container shadow-xl flex flex-col items-center 
+    <div>
+      <ToastContainer
+        position="top-center"
+        autoClose={20000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <div
+        className="bg-white container shadow-xl flex flex-col items-center 
           justify-between p-5 rounded-lg h-auto w-auto space-y-5"
-    >
-      <form onSubmit={handleSubmit(onSubmit)} className="">
-        {error && error.message}
+      >
+        <form onSubmit={handleSubmit(onSubmit)} className="">
+          {error && error.message}
 
-        {success && <p>Check your email</p>}
-        <h1 className="text-center text-lg font-bold ">Login</h1>
+          {success && <></>}
+          <h1 className="text-center text-lg font-bold ">Login</h1>
 
-        <p className="text-center text-sm pb-5">Welcome back to eGarage.</p>
+          <p className="text-center text-sm pb-5">Welcome back to eGarage.</p>
 
-        <div className="flex flex-col">
-          <input
-            type="email"
-            placeholder="jane.doe@example.com"
-            {...register("email")}
-            className="border border-[#118024]/50 w-96 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#118024] focus:border-transparent"
-          />
-          <button className="border w-24 mt-5 bg-[#118024]/90 text-white px-2 py-3 rounded-lg border-transparent">
-            Login
-          </button>
-        </div>
-      </form>
-      <Link href="/register">Register</Link>
+          <div className="flex flex-col">
+            <input
+              type="email"
+              placeholder="jane.doe@example.com"
+              {...register("email")}
+              className="border border-[#118024]/50 w-96 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#118024] focus:border-transparent"
+            />
+            <div className="flex justify-between items-center">
+              <button
+                className="border w-24 mt-5 bg-[#118024]/90 text-white px-2 py-3 rounded-lg border-transparent"
+                onSubmit={notify}
+              >
+                Login
+              </button>
+              <button className="flex items-center justify-center space-x-1 border-[#118024] border-2 w-24 px-2 py-3 mt-5 rounded-lg">
+                <ArrowRightIcon className="h-6 w-4 text-[#118024]" />
+                <Link href="/register">Register</Link>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
