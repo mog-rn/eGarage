e139b2ea079c4d73897eafc7b5ab0967b7c6a62f
mogakamo <mogaka.amo254@gmail.com>
2022-09-13 06:03:22 +0300

    tests: register page

---
+++ __tests__/register.test.tsx
@@ -0,0 +1,43 @@
+import RegisterPage from "../src/pages/register";
+import { cleanup, render, screen } from "@testing-library/react";
+import { trpc } from "../src/utils/trpc";
+
+// import trpc from '../src/utils/trpc';
+
+jest.mock("../src/utils/trpc");
+
+afterEach(cleanup);
+
+describe("Register", () => {
+  it("Displays register page content", async () => {
+    // render(<RegisterPage />);
+    // const heading = screen.getByRole("heading", {
+    // name: /Register/i,
+    // });
+    // expect(heading).toBeInTheDocument();
+  });
+});
+//
+// Compare this snippet from src/pages/register.tsx:
+// import dynamic from "next/dynamic";
+// import Link from "next/link";
+// import { useRouter } from "next/router";
+// import { useState } from "react";
+// import { useForm } from "react-hook-form";
+// import { CreateUserInput } from "../schema/user.schema";
+// import { trpc } from "../utils/trpc";
+//
+// const RegisterForm = dynamic(() => import("../components/RegisterForm"), {
+//   ssr: false,
+// });
+//
+// function RegisterPage() {
+//   return (
+//     <div className="h-screen flex items-center justify-center bg-[#F6FBF2]">
+//       <RegisterForm />
+//     </div>
+//   );
+// }
+//
+// export default RegisterPage;
+//
